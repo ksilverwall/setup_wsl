da8154e599ef472e24a76e2eadc656b577037045
Kazuki Tsunemi <kazuki_tsunemi@lockon.co.jp>
2018-08-17 13:28:40 +0900

    Remove docker

--- setup.sh
+++ setup.sh
@@ -15,17 +15,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 apt update
 apt install -y google-chrome-stable
 
-#install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-mv docker-compose /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-echo "export DISPLAY=localhost:0.0" >> ~/.bashrc
-echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
-
 # install phpenv
 curl -L http://git.io/phpenv-installer | bash
 echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bashrc
