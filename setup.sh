1f2f57555bdc7efbbf614b55a0ee57aa8b745061
Kazuki Tsunemi <kazuki_tsunemi@lockon.co.jp>
2018-08-27 15:40:14 +0900

    Fix setup scripts

--- setup.sh
+++ setup.sh
@@ -14,25 +14,14 @@ pip3 install awscli
 pip2 install boto3
 
 # other softwares
-ansible-playbook wsl_setup.yml
+ansible-playbook wsl_setup.yml -K
 
 ##############################
 # Should be ansibled
 
 dpkg-reconfigure openssh-server
-apt install -y x11-apps xinit
+sudo apt install -y x11-apps xinit
 
-# install google chrome
-bash -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-apt update
-apt install -y google-chrome-stable
-
-# install phpenv
-curl -L http://git.io/phpenv-installer | bash
-echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(phpenv init -)"' >> ~/.bashrc
-
-apt install -y libssl-dev libmcrypt-dev libreadline-dev libxslt1-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libpng-dev libjpeg-dev libmcrypt-dev libsqlite-dev libtidy-dev libltdl-dev make autoconf automake re2c lemon
-apt install -y libevent-dev -K
+sudo apt install -y libssl-dev libmcrypt-dev libreadline-dev libxslt1-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libpng-dev libjpeg-dev libmcrypt-dev libsqlite-dev libtidy-dev libltdl-dev make autoconf automake re2c lemon
+sudo apt install -y libevent-dev -K
 
